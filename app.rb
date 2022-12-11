2869b4f64af04c09c4dfd69b8f1ae3b973c46b38
Kevin Diesenberg <kdiesenb@gmail.com>
2022-12-11 10:30:49 -0500

    commit tree c5718870621cecfda5e0bdae102d8faac653b7f4

---
+++ app.rb
@@ -0,0 +1,37 @@
+#this is a comment
+# puts "Hello, World!"
+# puts("hello world!")
+
+# puts "adds a line break at the end of the string"
+
+# print "does just about the same thing"
+
+# print "hellow worlds"
+
+# print "hello world"
+
+# put and print work for strings
+
+# p and pp inspect other kinds of data
+
+# puts [1, 2, 3]
+
+# puts coerces data to a string
+
+# .to_s method returns a string
+
+# puts [1, 2, 3].inspect
+
+# p [1, 2, 3]
+# p calls .inspect method on data
+# use pp, pretty pringting, on complex data, nasted arrays, hashes..hashes are equiv js object
+
+# pp [{id: 1, hello: "world"}, {id: 2, hello: "ruby"}, {id: 3, hello: "moon"} ]
+# .pretty_inspect method on data
+
+# puts method ALWAYS returns nil
+puts "Hello World!"
+
+print "Pass this test, please."
+
+p [1, 2, 3]
